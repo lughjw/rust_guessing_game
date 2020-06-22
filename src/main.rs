c0c9ef105d6bee8132d06631b22ed9579b483d3d
lughjw <lughjw@gmail.com>
2020-06-22 14:40:23 -0500

    finished program including guess_count

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,40 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    let target = rand::thread_rng().gen_range(1, 101);
+    let mut guesses_cnt: u32 = 0;
 
-    println!("You guessed: {}", guess);
-    
+    println!("Guess the number!");
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        guesses_cnt += 1;
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            // {
+            //     guesses_cnt+=1;
+            //     return num;
+            // },
+            Err(_) => continue,
+        };
+            
+
+        println!("You guessed: {}", guess);
+        
+        match guess.cmp(&target) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win after {} guesses!", guesses_cnt);
+                break;
+            }
+        }
+    }
 }
+
