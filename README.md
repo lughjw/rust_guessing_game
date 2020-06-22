feac9c8e9518aee1a1d1b351e443dcf82fb25fb3
lughjw <lughjw@gmail.com>
2020-06-22 16:40:29 -0500

    added an example about addressing to both code and readme.

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ Basic guessing game created to learn rust
 * You can get the address for a variable using ```&```.<br>
 ```Rust
 let foo = 0;
-// Print the address of foo.
+// Both of these print the value of foo
+println!("{}", foo);
 println!("{}", &foo);
+
+// Print the address of foo (p stands for pointer)
+println!("{:p}", &foo);
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     let target = rand::thread_rng().gen_range(1, 101);
     let mut guesses_cnt: u32 = 0;
+    
+    // let foo = 0;
+    // println!("foo is {}", foo);
+    // println!("foo is {}", &foo);
+    // println!("foo is {:p}", &foo);
 
     println!("Guess the number!");
+
     loop {
         println!("Please input your guess.");
+
         let mut guess = String::new();
 
         io::stdin()
@@ -16,24 +23,22 @@ fn main() {
             .expect("Failed to read line");
 
         guesses_cnt += 1;
-        
+
         // Convert to a number (32 bit signed integer: i32)
         let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-            
 
         println!("You guessed: {}", guess);
-        
+
         match guess.cmp(&target) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win after {} guesses!", guesses_cnt);
+                println!("You win after {} {}!", guesses_cnt, if guesses_cnt > 1 {"guesses"} else {"lucky guess"});
                 break;
             }
         }
     }
 }
-
